02420df68907a59688813fcb55cc85b6ec75a6b6
dd84ai <dd84ai@gmail.com>
2021-03-21 11:55:06 +0700

    feat: urls, until further notice setting admin page as default

--- FLDarknet/fldarknet/urls.py
+++ FLDarknet/fldarknet/urls.py
@@ -20,8 +20,8 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    #path('admin/', admin.site.urls),
     
-    path('polls/', include('polls.urls')),
+    #path('polls/', include('polls.urls')),
     path('', admin.site.urls),
 ]
