4b030c8f896700ec30ea75e4bc02b6c09ff37389
dd84ai <dd84ai@gmail.com>
2021-04-03 19:53:53 +0700

    feat(scripts): a way to launch plint at pc

---
+++ FLDarknet/scripts/fldarknet_pylint.bat
@@ -0,0 +1,2 @@
+
+cmd /k "cd /d ..\venv\Scripts & activate & cd /d    ..\.. & pylint --load-plugins pylint_django --django-settings-module="fldarknet.settings" --disable=django-not-configured fldarknet main ship commodities"
