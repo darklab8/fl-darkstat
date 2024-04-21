e45ebe7c85dfa8436ad410e9b139f5e45eecd4a8
dd84ai <dd84ai@gmail.com>
2024-04-22 01:11:02 +0200

    feat: filtering bar added

--- darkstat/front/static_front/custom.js
+++ darkstat/front/static_front/custom.js
@@ -22,3 +22,23 @@ function Unhide(id) {
     console.log("Unhide.id=" + id)
     element.classList.remove("hidden");
 }
+
+function FilteringFunction() {
+    // Declare variables
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("filterinput");
+    filter = input.value.toUpperCase();
+    table = document.querySelector("#table-top table");
+    tr = table.getElementsByTagName("tr");
+
+    // Loop through all table rows, and hide those who don't match the search query
+    for (i = 1; i < tr.length; i++) {
+        row = tr[i];
+        txtValue = row.textContent || row.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            tr[i].style.display = "";
+        } else {
+            tr[i].style.display = "none";
+        }
+    }
+}
