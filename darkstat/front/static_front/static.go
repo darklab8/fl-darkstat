12a6a1aa38f5e555e84d8b6b136e0edbc7edfa4d
dd84ai <dd84ai@gmail.com>
2024-05-09 16:36:47 +0200

    docs: add mention about htmx commented out stuff

--- darkstat/front/static_front/static.go
+++ darkstat/front/static_front/static.go
@@ -4,6 +4,14 @@ import (
 	_ "embed"
 )
 
+// Commented out IE stuff as it makes things slow
+// if (element.children) { // IE
+//
+//		forEach(element.children, function(child) { cleanUpElement(child) });
+//	}
+//
+// see https://github.com/bigskysoftware/htmx/issues/879 for more details
+//
 //go:embed htmx.1.9.11.min.js
 var HtmxMinJs string
 
