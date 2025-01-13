4fa508bcaf5585016df93acf9429bc7e17b5f034
dd84ai <dd84ai@gmail.com>
2025-01-13 07:05:34 +0100

    refactor: standartize app naming

--- tf/modules/darkstat/main.tf
+++ tf/modules/darkstat/main.tf
@@ -14,7 +14,7 @@ data "docker_network" "caddy" {
 }
 
 resource "docker_service" "darkstat" {
-  name = "${var.environment}-darkstat-web"
+  name = "${var.environment}-darkstat-app"
 
   task_spec {
     networks_advanced {
