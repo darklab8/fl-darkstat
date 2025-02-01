a02ef840ea82f0a179b18ac03264d4f615e2de7b
dd84ai <dd84ai@gmail.com>
2025-02-01 22:18:46 +0100

    test: cover infocard api reading too

--- darkapi/api_test.go
+++ darkapi/api_test.go
@@ -213,6 +213,34 @@ func TestApi(t *testing.T) {
 
 			assert.Nil(t, items[0].Error)
 		})
+
+		t.Run("GetInfocards", func(t *testing.T) {
+			var nickname []string = []string{
+				items[0].GetNickname(),
+				items[1].GetNickname(),
+				"smth that does not exist",
+			}
+
+			post_body, err := json.Marshal(nickname)
+			logus.Log.CheckPanic(err, "unable to marshal post body", typelog.OptError(err))
+
+			res, err := httpc.Post("http://localhost/api"+"/infocards", ApplicationJson, bytes.NewBuffer(post_body))
+			logus.Log.CheckPanic(err, "error making http request: %s\n", typelog.OptError(err))
+
+			resBody, err := io.ReadAll(res.Body)
+			logus.Log.CheckPanic(err, "client: could not read response body: %s\n", typelog.OptError(err))
+
+			var items []InfocardResp
+			fmt.Println("resBody=", string(resBody))
+			err = json.Unmarshal(resBody, &items)
+			logus.Log.CheckPanic(err, "can not unmarshal", typelog.OptError(err))
+
+			assert.Greater(t, len(items), 0)
+
+			assert.Nil(t, items[0].Error)
+			assert.Nil(t, items[1].Error)
+			assert.NotNil(t, items[2].Error)
+		})
 	})
 
 	t.Run("GetCommodities", func(t *testing.T) {
