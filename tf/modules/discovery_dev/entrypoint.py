ae95dee91c24f98a8f65190dd38b0cc1337cdc03
dd84ai <dd84ai@gmail.com>
2025-09-16 02:33:08 +0200

    ops: adjust status=updating ping :)

--- tf/modules/discovery_dev/entrypoint.py
+++ tf/modules/discovery_dev/entrypoint.py
@@ -24,6 +24,10 @@ while True:
         os.system(f'docker service update --env-add DARKCORE_PASSWORD={NEW_PASSWORD} --env-add "FLDARKSTAT_HEADING=commit:{NEW_PASSWORD}" --image darkwind8/darkstat:production dev-darkstat-app')
         OLD_PASSWORD=NEW_PASSWORD
         STATE=json.loads(subprocess.run("docker service inspect dev-darkstat-app", shell=True, capture_output=True).stdout.decode("utf8").replace("\n",""))[0]["UpdateStatus"]["State"]
+        if "updating" in STATE:
+            time.sleep(10)
+            STATE=json.loads(subprocess.run("docker service inspect dev-darkstat-app", shell=True, capture_output=True).stdout.decode("utf8").replace("\n",""))[0]["UpdateStatus"]["State"]
+
         content_msg = f"https://darkstat-dev.dd84ai.com/?password={NEW_PASSWORD} state={STATE}"
         if "state=completed" not in content_msg:
             content_msg += " <@370435997974134785>"
