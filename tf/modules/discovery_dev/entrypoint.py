bea20e9016fe3344922d1f86c8b8c329d19676d5
dd84ai <dd84ai@gmail.com>
2025-09-16 02:34:40 +0200

    refactor: fix updatin gproblem for sure

--- tf/modules/discovery_dev/entrypoint.py
+++ tf/modules/discovery_dev/entrypoint.py
@@ -24,7 +24,7 @@ while True:
         os.system(f'docker service update --env-add DARKCORE_PASSWORD={NEW_PASSWORD} --env-add "FLDARKSTAT_HEADING=commit:{NEW_PASSWORD}" --image darkwind8/darkstat:production dev-darkstat-app')
         OLD_PASSWORD=NEW_PASSWORD
         STATE=json.loads(subprocess.run("docker service inspect dev-darkstat-app", shell=True, capture_output=True).stdout.decode("utf8").replace("\n",""))[0]["UpdateStatus"]["State"]
-        if "updating" in STATE:
+        while "updating" in STATE:
             time.sleep(10)
             STATE=json.loads(subprocess.run("docker service inspect dev-darkstat-app", shell=True, capture_output=True).stdout.decode("utf8").replace("\n",""))[0]["UpdateStatus"]["State"]
 
