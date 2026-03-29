919357757a020e5b4d25573b42b96e7705d2749b
dd84ai <dd84ai@gmail.com>
2026-03-29 19:34:57 +0200

    fix: repair system connection highlighter

--- darkmap/front/static_front/map_galaxy.js
+++ darkmap/front/static_front/map_galaxy.js
@@ -1,3 +1,31 @@
+function InstallEdgeHighlight() {
+    var systems = document.querySelectorAll("system-");
+    for (let row = 0; row < systems.length; row++) {
+        systems[row].addEventListener('mouseover', function () {
+            let system_nickname = systems[row].attributes["nickname"].value;
+            let lines1 = document.querySelectorAll('line[data-system2-nickname="' + system_nickname + '"]');
+            let lines2 = document.querySelectorAll('line[data-system1-nickname="' + system_nickname + '"]');
+            for (let i = 0; i < lines1.length; i++) {
+                lines1[i].classList.add("conn_hover");
+            }
+            for (let i = 0; i < lines2.length; i++) {
+                lines2[i].classList.add("conn_hover");
+            }
+        });
+        systems[row].addEventListener('mouseout', function () {
+            let system_nickname = systems[row].attributes["nickname"].value;
+            let lines1 = document.querySelectorAll('line[data-system2-nickname="' + system_nickname + '"]');
+            let lines2 = document.querySelectorAll('line[data-system1-nickname="' + system_nickname + '"]');
+            for (let i = 0; i < lines1.length; i++) {
+                lines1[i].classList.remove("conn_hover");
+            }
+            for (let i = 0; i < lines2.length; i++) {
+                lines2[i].classList.remove("conn_hover");
+            }
+        });
+    }
+}
+
 function InstallGalaxy() {
     InstallLabelOverlapper();
     InstallMenu();
