4a9c4ae3363de240eb692c2bec71f7211d104095
dd84ai <dd84ai@gmail.com>
2026-05-05 22:48:40 +0200

    feat: make sure links remain to objects

--- darkmap/front/static_front/custom.js
+++ darkmap/front/static_front/custom.js
@@ -304,3 +304,11 @@ function LabelsReset() {
 function CloseInfocard() {
     document.querySelector("#remodal-bg").style.display = "none";
 }
+
+function updateQuery(el) {
+    const url = new URL(window.location.href);
+    url.searchParams.set('q', el.dataset.nickname);
+    window.history.pushState({}, '', url);
+
+    document.querySelectorAll('.is-target').forEach(obj => obj.classList.remove('is-target'));
+}
