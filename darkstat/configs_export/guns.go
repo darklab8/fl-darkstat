a9a1607df63fefdfd06b667dad330de17713017c
dd84ai <dd84ai@gmail.com>
2026-07-24 00:25:35 +0200

    fix: check motor for nil

--- darkstat/configs_export/guns.go
+++ darkstat/configs_export/guns.go
@@ -269,7 +269,9 @@ func (e *Exporter) getGunInfo(gun_info *equip_mapped.Gun, ids []*Tractor, buyabl
 
 		if motor_nick, ok := munition.Motor.GetValue(); ok {
 			motor := e.Mapped.Equip().MotorMap[motor_nick]
-			e.WriteConfigToInfocard(&motor.Model, gun.Nickname)
+			if motor != nil {
+				e.WriteConfigToInfocard(&motor.Model, gun.Nickname)
+			}
 		}
 
 		e.first_time_adding_gun[gun_nickname] = true
